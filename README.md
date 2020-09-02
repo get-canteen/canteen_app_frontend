f4c262c65a015f254cc545c2658ad96a39d87299
Donald Chen <donaldchen@Donalds-MacBook-Pro.local>
2020-09-02 12:41:55 -0700

    Fix typos within CloudFunctionManager

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # canteen_app_frontend
 Frontend for Canteen web app
 
+# Getting Started
+1. Clone the repo.
+2.  
--- src/functions/functions.js
+++ src/functions/functions.js
@@ -3,14 +3,14 @@ import { firebase } from '../../firebase/firebase';
 export class CloudFunctionManager {
     static addRequest = firebase.functions().httpsCallable('addRequest')
 
-    static getRecommendations = firebase.functions().httpsCallable('joinGroup')
+    static getRecommendations = firebase.functions().httpsCallable('getRecommendations')
   
-    static declineRecommendation = firebase.functions().httpsCallable('joinGroup')
+    static declineRecommendation = firebase.functions().httpsCallable('declineRecommendation')
   
-    static acceptRecommendation = firebase.functions().httpsCallable('joinGroup')
+    static acceptRecommendation = firebase.functions().httpsCallable('acceptRecommendation')
   
     static joinGroup = firebase.functions().httpsCallable('joinGroup')
   
-    static getQueryApiKey = firebase.functions().httpsCallable('joinGroup')
+    static getQueryApiKey = firebase.functions().httpsCallable('joinQueryApiKey')
 }
   
