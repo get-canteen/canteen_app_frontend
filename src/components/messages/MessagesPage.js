fe89b40de84e6332d534bce7a9dc3a1069120c67
Sarah Nessa <sarahnessaa@gmail.com>
2020-09-11 12:59:13 -0700

    Initial file

--- src/components/messages/MessagesPage.js
+++ src/components/messages/MessagesPage.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { startFetchMessages } from '../../actions/matches';
 
-const MessagesPage = () => (
-    <div>
-        <h1> Messages Page </h1>
-    </div>
-);
 
-export default MessagesPage;
+class MessagesPage extends React.Component {
+    state = {
+        chats: [],
+        content: ''
+    }
+
+    onInputChange = (e) => {
+        const content = e.target.value;
+        this.state.chats.push(content);
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.startFetchMessages();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Messages Page </h1>
+                <div className="chats">
+                    <p className="timestamp"></p>
+                    <p className="message-request">
+                    </p>
+                    {this.state.chats.map(chat => {
+                        return <p key={chat.index}>{chat.content}</p>
+                    })}
+                </div>
+                <form onSubmit={this.props.startFetchMessages}>
+                    <input 
+                        placeholder="Send a message..." 
+                        onChange={this.onInputChange} 
+                        value={this.state.content}>
+                    </input>
+                    {this.state.error ? <p>{this.state.error.message}</p> : null}
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    setMessages: state.auth.setMessages
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    startFetchMessages: () => dispatch(startFetchMessages())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessagesPage);
